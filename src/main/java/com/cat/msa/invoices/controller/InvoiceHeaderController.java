fc88455acac035c7a8d025f94f431459698be6e1
KevinMoscoso <sebastianbaculima1@gmail.com>
2025-05-17 12:03:37 -0500

    feat: find all invoices

--- src/main/java/com/cat/msa/invoices/controller/InvoiceHeaderController.java
+++ src/main/java/com/cat/msa/invoices/controller/InvoiceHeaderController.java
@@ -5,6 +5,7 @@ import com.cat.msa.invoices.service.InvoiceHeaderService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @RestController
 public class InvoiceHeaderController implements InvoiceHeaderApi{
@@ -20,4 +21,11 @@ public class InvoiceHeaderController implements InvoiceHeaderApi{
         InvoiceHeader savedInvoiceHeader = invoiceHeaderService.createInvoiceHeader(invoiceHeader);
         return new ResponseEntity<>(savedInvoiceHeader, HttpStatus.CREATED);
     }
+
+    @Override
+    public ResponseEntity<List<InvoiceHeader>> findAll() {
+        List<InvoiceHeader> invoiceHeaders = invoiceHeaderService.getAll();
+        return new ResponseEntity<>(invoiceHeaders, HttpStatus.OK);
+    }
+
 }
--- src/main/java/com/cat/msa/invoices/service/InvoiceHeaderService.java
+++ src/main/java/com/cat/msa/invoices/service/InvoiceHeaderService.java
@@ -1,7 +1,9 @@
 package com.cat.msa.invoices.service;
 
 import com.cat.msa.invoices.domain.InvoiceHeader;
+import java.util.List;
 
 public interface InvoiceHeaderService {
     InvoiceHeader createInvoiceHeader(InvoiceHeader invoiceHeader);
+    List<InvoiceHeader> getAll();
 }
